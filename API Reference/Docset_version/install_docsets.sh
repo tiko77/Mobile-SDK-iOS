ad3d4d1c056028657a4e92cdb2fc76db3fc13c65
tiko77 <dash7imo@gmail.com>
2016-03-19 13:08:06 -0300

    Update install_docsets.sh

--- API Reference/Docset_version/install_docsets.sh
+++ API Reference/Docset_version/install_docsets.sh
@@ -13,3 +13,28 @@ echo "Installation complete. Please restart Xcode."
 break;
 
 )
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // disable the connect button by default
+    [self.connectButton setEnabled:NO];
+
+    //Register App with key
+    NSString* appKey = @"Please enter your App Key here";
+    [DJISDKManager registerApp:appKey withDelegate:self];
+
+    self.sdkVersionLabel.text = [@"DJI SDK Version: " stringByAppendingString:[DJISDKManager getSDKVersion]];
+}- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // disable the connect button by default
+    [self.connectButton setEnabled:NO];
+
+    //Register App with key
+    NSString* appKey = @"Please enter your App Key here";
+    [DJISDKManager registerApp:appKey withDelegate:self];
+
+    self.sdkVersionLabel.text = [@"DJI SDK Version: " stringByAppendingString:[DJISDKManager getSDKVersion]];
+}
