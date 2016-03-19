e409f5c5d6a8a1c7eab697379ff78078e1731b9a
tiko77 <dash7imo@gmail.com>
2016-03-19 13:06:16 -0300

    Update install_docsets.sh

--- API Reference/Docset_version/install_docsets.sh
+++ API Reference/Docset_version/install_docsets.sh
@@ -1,15 +1,12 @@
-#!/bin/sh
-#
-# Copyright (c) 2015-present, DJI, Inc. All rights reserved.
-#
-# This script installs the docsets in the current folder into the user's ~/Library
+- (void)viewDidLoad {
+    [super viewDidLoad];
 
-(
-cd "$(dirname "$0")"
-DESTINATION="$HOME/Library/Developer/Shared/Documentation/DocSets/"
-echo "This script will install the docsets in the current folder into $DESTINATION"
-\ls -d *.docset | xargs -I {} cp -R {} $DESTINATION
-echo "Installation complete. Please restart Xcode."
-break;
+    // disable the connect button by default
+    [self.connectButton setEnabled:NO];
 
-)
+    //Register App with key
+    NSString* appKey = @"Please enter your App Key here";
+    [DJISDKManager registerApp:appKey withDelegate:self];
+
+    self.sdkVersionLabel.text = [@"DJI SDK Version: " stringByAppendingString:[DJISDKManager getSDKVersion]];
+}
